e8edc3df4bd80f94fbb8f5411cec692c198c7f44
Sofyan Sugianto <sofyanartem@gmail.com>
2023-06-17 13:14:50 +0700

    allow force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+	if ($this->checkHttpStatus()) {
+            \URL::forceScheme('https');
+	}
         //
+    }
+
+    private function checkHttpStatus(){
+	return ((isset($_SERVER['HTTP_X_FORWARDED_PROTO'])&&$_SERVER['HTTP_X_FORWARDED_PROTO']==='https') || \App::environment(['staging', 'production']));
     }
 }
